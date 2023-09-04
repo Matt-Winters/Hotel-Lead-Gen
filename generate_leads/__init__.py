505a5f5ba6b7cd1ddb62304f23467d1a791805ed
Matt Winters <winters.matt96@gmail.com>
2023-09-04 17:38:00 -0400

    wrapped up full flow

--- generate_leads/__init__.py
+++ generate_leads/__init__.py
@@ -7,10 +7,14 @@ from generate_leads.main import run_script
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    pararms = req.params
+    pararms = req.params.get("region")
 
     try:
-        run_script()
+        data = run_script(pararms)
+        return func.HttpResponse(
+            data,
+            200
+        )
         
     except Exception as e:
         return func.HttpResponse(
--- generate_leads/main.py
+++ generate_leads/main.py
@@ -1,18 +1,44 @@
 from google_hotel import flow as google_flow
 from travel_weekly import flow as tw_flow
+import csv
+import io
 
 
-def convert_to_csv(data):
-    return data
+def convert_to_csv(hotels):
+    # Create an in-memory file-like object
+    memory_file = io.StringIO()
+    
+    # Create a CSV writer using the memory file
+    writer = csv.writer(memory_file)
+    
+    # Write the header row
+    writer.writerow(['Name', 'Rooms', 'Rate'])
+    
+    # Write the hotel data rows
+    for hotel in hotels:
+        writer.writerow([hotel.name, hotel.rooms, hotel.rate])
+    
+    # Move the file pointer to the beginning of the memory file
+    memory_file.seek(0)
+    
+    # Read the contents of the memory file
+    csv_contents = memory_file.getvalue()
+    
+    return csv_contents
 
 
-def run_script(region):
+def run_script(region) -> str:
 
     list_of_hotels = google_flow.run(region)
 
-    data = tw_flow.run(list_of_hotels)
+    list_of_hotels = tw_flow.run(list_of_hotels)
 
-    csv = convert_to_csv(data)
-
+    data = convert_to_csv(list_of_hotels)
         
-    return csv
+    return data
+
+if __name__ == '__main__':
+    from google_hotel.flow import Hotel
+    hotels = [Hotel('Market Pavilion Hotel')]
+    data = convert_to_csv(hotels)
+    print('here')
